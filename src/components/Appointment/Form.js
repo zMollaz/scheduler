a3b8d9348671d0c7dfa354e3c68e8242d98091d3
A E <aelmollaz@gmail.com>
2022-01-19 23:41:20 -0500

    Added show & header components to Appointment component & stories

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
@@ -12,35 +12,39 @@ const Form = (props) => {
   const cancel = () => {
     reset();
     props.onCancel();
-  }
-  
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
-    <form onSubmit={(event) => event.preventDefault()} autoComplete="off">
-      <input
-        className="appointment__create-input text--semi-bold"
-        name="name"
-        type="text"
-        placeholder="Enter Student Name"
-        value={student}
-        onChange={(event) => setStudent(event.target.value) }
-      />
-    </form>
-    <InterviewerList
-      interviewers={props.interviewers}
-      value={interviewer}
-      onChange={setInterviewer}
-    />
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <Button danger onClick={cancel}>Cancel</Button>
-      <Button confirm onClick={props.onSave}>Save</Button>
-    </section>
-  </section>
-</main>
-  )
-}
+      <section className="appointment__card-left">
+        <form onSubmit={(event) => event.preventDefault()} autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={props.onSave}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+};
 
 export default Form;
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,22 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 import "./styles.scss";
 
 function Appointment(props) {
+  console.log(props)
+  const { time, interview } = props;
   return (
     <article className="appointment">
-      
-      {props.time ? `Appointment at ${props.time}` : "No Appointments"}
+      <Header time={time} />
+      {interview ? (
+        <Show student={interview.student} interviewer={interview.interviewer} />
+      ) : (
+        <Empty />
+      )}
     </article>
-  )
+  );
 }
 
 export default Appointment;
