44a3c317b11fb9e9b422174cf773e986f986ae71
A E <aelmollaz@gmail.com>
2022-01-21 09:00:50 -0500

    Added useEffect & axios to Application to fetch days from db

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
@@ -42,26 +42,17 @@ const appointments = [
     time: "4pm",
   },
 ];
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState("Monday");// mayneed to delete this
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get("/api/days").then((response) => {
+      console.log("This is res ", response);
+      setDays(response.data);
+    })
+  },[])
+
   return (
     <main className="layout">
       <section className="sidebar">
